009d46d21ed3593d9b61317788c9a99d245c293f
Maxwell <lubalecliff@gmail.com>
2023-12-17 21:56:48 +0300

    Completed Assignment

--- lib/debug.py
+++ lib/debug.py
@@ -1,8 +1,23 @@
-#!/usr/bin/env python3
-
 from config import CONN, CURSOR
 from song import Song
 
-
 if __name__ == '__main__':
-    import ipdb; ipdb.set_trace()
+    try:
+        # Create the 'songs' table if it doesn't exist
+        Song.create_table()
+
+        # Create and save some songs
+        hello = Song.create("Hello", "25")
+        despacito = Song.create("Despacito", "Vida")
+
+        # Access the IDs of the saved songs
+        print(hello.id)
+        print(despacito.id)
+
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        # Commit changes and close the connection
+        CONN.commit()
+        CONN.close()
+
--- lib/song.py
+++ lib/song.py
@@ -1,4 +1,39 @@
 from config import CONN, CURSOR
 
 class Song:
-    pass
+    def __init__(self, name, album):
+        self.id = None
+        self.name = name
+        self.album = album
+    
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS songs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                album TEXT
+            )
+        """
+        with CONN:  # Use 'with' statement to manage the cursor
+            CURSOR.execute(sql)
+    
+    def save(self):
+        sql = """
+            INSERT INTO songs (name, album)
+            VALUES (?, ?)
+        """
+        try:
+            with CONN:
+                CURSOR.execute(sql, (self.name, self.album))
+                result = CURSOR.execute("SELECT last_insert_rowid() FROM songs").fetchone()
+                if result:
+                    self.id = result[0]
+        except Exception as e:
+            print(f"Error in save method: {e}")
+
+    @classmethod
+    def create(cls, name, album):
+        song = cls(name, album)
+        song.save()
+        return song
